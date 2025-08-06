3890a6042bb9c72e8546cfd0df8c4d475ff6fd83
MOHMOHSAN <moh.moh.san@xylem.com>
2025-08-06 16:19:08 +0800

    "Updating the logic to put Mine and calculate adjacentMinesCount near mine"

--- src/main/java/minesweeper/Board.java
+++ src/main/java/minesweeper/Board.java
@@ -1,10 +1,15 @@
 package main.java.minesweeper;
 
+import java.util.Random;
+
 public class Board {
     private final int rows;
     private final int cols;
     private final int noOfMines;
     private final Cell[][] cellGrid;
+
+    int [] dx = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
+    int [] dy = {0, 0, 0, -1, -1, -1, 1, 1, 1};
 
     public Board(int boardSize, int noOfMines){
         this.rows = boardSize;
@@ -14,12 +19,63 @@ public class Board {
         createBoardWithMines();
     }
 
-    public void createBoardWithMines(){
+    private void createBoardWithMines(){
         for(int i=0; i < rows; i++){
             for(int j=0; j < cols; j++){
                 cellGrid[i][j] = new Cell();
             }
         }
+        setMinesField();
+        setAllAdjacentMinesCount();
+    }
+
+    // Generate radom cell grid index and set isFlag value to that cell to be reserved as Mine
+    private void setMinesField(){
+        Random rand = new Random();
+        int successfullyPlantedMinesCount = 0;
+        while(successfullyPlantedMinesCount < noOfMines) {
+            int row = rand.nextInt(rows);
+            int col = rand.nextInt(cols);
+            if(!cellGrid[row][col].isFlag()){
+                cellGrid[row][col].setFlag(true);
+                successfullyPlantedMinesCount++;
+            }
+        }
+    }
+
+    // To check if the current grid is valid
+    private boolean isValidGrid(int rowIndex, int colIndex) {
+        return rowIndex >= 0 && rowIndex < rows && colIndex >=0 && colIndex < cols;
+    }
+
+    // Get All mines count at the adjacent cells except on mine cell
+    private void setAllAdjacentMinesCount(){
+        for(int i=0; i < rows; i++){
+            for(int j=0; j < cols; j++){
+                Cell currentCell = cellGrid[i][j];
+                if(!currentCell.isFlag()){
+                    int getAdjacentMinesCountForCurrGrid = countAdjacentMinesForCurrGrid(i,j);
+                    cellGrid[i][j].setAdjacentMinesCount(getAdjacentMinesCountForCurrGrid);
+                }
+            }
+        }
+    }
+
+    // To get the adjacentMines Count from eight direction based on current grid
+    private int countAdjacentMinesForCurrGrid(int rowIndex, int colIndex){
+        int count = 0;
+        for(int i=0; i < dx.length; i++){
+            int xDirIndexForCurrGrid = rowIndex + dx[i];
+            int yDirIndexForNextGrid = colIndex + dy[i];
+
+            //Check grid from eight direction is valid and has mine
+            if(isValidGrid(xDirIndexForCurrGrid, yDirIndexForNextGrid) &&
+                    cellGrid[xDirIndexForCurrGrid][yDirIndexForNextGrid].isFlag()
+            ){
+                count++;
+            }
+        }
+        return count;
     }
 
     public void showBoard(){
--- src/main/java/minesweeper/Cell.java
+++ src/main/java/minesweeper/Cell.java
@@ -1,9 +1,31 @@
 package main.java.minesweeper;
 
 public class Cell {
+    private boolean isFlag = false;
+    private int adjacentMinesCount = 0;
+
+    // Getter and Setter for isFlag
+    public boolean isFlag() {
+        return isFlag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.isFlag = flag;
+    }
+
+    // Getter and Setter for adjacentMinesCount
+    public int getAdjacentMinesCount() {
+        return adjacentMinesCount;
+    }
+
+    public void setAdjacentMinesCount(int adjacentMinesCount) {
+        this.adjacentMinesCount = adjacentMinesCount;
+    }
 
     @Override
     public String toString(){
+        if(isFlag){ return "f"; }
+        if(adjacentMinesCount != 0){ return adjacentMinesCount+""; }
         return "_";
     }
 }
